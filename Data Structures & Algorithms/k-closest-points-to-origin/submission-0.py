98caca7befa70c32bfeb5b23a92299ce2e49b4ed
Kuntal Pal <kpal002@ucr.edu>
2026-05-26 15:12:17 -0700

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def kClosest(self, points, k):
+
+        # sort by squared distance from origin
+
+        points.sort(key=lambda p: p[0]*p[0] + p[1]*p[1])
+
+        return points[:k]
+
+        
+        
